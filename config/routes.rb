e3c972fe12af131ec091217d638aa12b8cbd3f7d
Thennavan Ramalingam <tramalingam@lat-tramalingam.local>
2015-11-25 01:40:10 -0500

    adding images and album pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'contactus/index'
+  get 'aboutus/index'
+  get 'gallery/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
