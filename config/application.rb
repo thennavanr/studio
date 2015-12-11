346186e3dfc3ab72fca52db6a7942a0dad6f6a9c
Thennavan Ramalingam <thennavanr@gmail.com>
2015-12-11 18:31:01 -0500

    heroku rails image fight

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Studio
     config.assets.enabled = true
     config.assets.version = 1.0
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
