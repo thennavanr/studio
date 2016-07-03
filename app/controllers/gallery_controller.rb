9faff98f3523191b5c2a52d384ec59c1a18da96d
Thennavan Ramalingam <thennavanr@gmail.com>
2016-07-03 00:14:20 -0400

    testing filter

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,6 +1,8 @@
 class GalleryController < ApplicationController
   def index
     @albums =[]
+    filter = params[:filter]
+    filter = 'bd'
     Aws.config[:credentials] = Aws::Credentials.new( ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
     s3 = Aws::S3::Resource.new(credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]),region:'us-east-1')
     obj  = s3.bucket("backdrop-studio")
@@ -13,7 +15,11 @@ class GalleryController < ApplicationController
         album_name = o.key.split("gallery/clients/image/")[1]
         if album_name
           album_name = album_name.split('/')[0]
-          stat1 = AlbumStat.where({:album => album_name}).first
+          if filter
+            stat1 = AlbumStat.where("album =  '#{album_name}' and  tags like '%#{filter}%' "  ).first
+          else
+            stat1 = AlbumStat.where(:album => album_name).first
+          end 
           val['url'] = url
           if stat1
             val['views'] = stat1.views 
@@ -30,7 +36,8 @@ class GalleryController < ApplicationController
             val['album']=album_name
           end
         end
-        @albums << val
+        
+        @albums << val if stat1
       end
     end
 
